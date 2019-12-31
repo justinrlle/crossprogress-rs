3d3fa0c1c9ccd80629856d58b0f496bf08b7c239
JustinRlle <justin.rerolle@protonmail.com>
2019-12-31 14:50:35 +0100

    move main to examples

--- examples/simple.rs
+++ examples/simple.rs
@@ -5,12 +5,10 @@ use std::{
 };
 
 fn main() {
-    let (columns, rows) = crossterm::terminal::size().unwrap();
-    println!("terminal: {} columns, {} rows", columns, rows);
     let mut stdout = io::stdout();
     let mut bar = crossprogress::percent_bar(50).build(&mut stdout).unwrap();
     for _ in 0..50 {
-        thread::sleep(Duration::from_millis(1000));
+        thread::sleep(Duration::from_millis(200));
         bar.inc(1).unwrap();
     }
 }
